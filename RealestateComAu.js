87d65c0c688c33ee1ffff68625b25752da16cd35
Chris Malone <chrismrulz@hotmail.com>
2023-05-31 20:46:56 +1000

    Add files via upload

--- RealestateComAu.js
+++ RealestateComAu.js
@@ -1,4 +1,4 @@
-const express = require('node-fetch-commonjs');
+const fetch = require('node-fetch-commonjs');
 const randomUserAgents = [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 ];
